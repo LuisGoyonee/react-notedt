925b7fefdc0dd23b31b17a03ee39a8b95eb439fd
Luis Michael Goyone <luismichaelgoyone@gmail.com>
2022-05-05 04:07:58 +0800

    Revert "chore: trying out chartjs"
    This reverts commit f33d8c426bdc30c785a4d06ab85488b78ae79542.

--- src/pages/Transactions/transactions.js
+++ src/pages/Transactions/transactions.js
@@ -14,58 +14,61 @@ const Transactions = () => {
   return (
     <>
       {checkTransactions ? (
-        <div className="transactionContainer">
-          <div className="header uppercase">Transaction details</div>
-          <div className="transactionItemsContainer">
-            {transactions.map((item, index) => {
-              const { date, amount, description, category, transactionType } =
-                item;
-              return (
-                <div className="transactionItem" key={index}>
-                  <div className="leftSideContainer">
-                    <div className="transaction-item-date font-normal text-xl">
-                      {moment(date).format("D dddd, MMMM  Y")}
-                    </div>
-                    <div className="pair">
-                      <IconContext.Provider
-                        value={{
-                          color:
-                            transactionType === "Income"
-                              ? "#00766C"
-                              : "#760000",
-                          className: "global-class-name transaction-item-icon",
-                        }}
-                      >
-                        <div>
-                          <BsCircleFill />
+        <>
+          <div className="transactionContainer">
+            <div className="header uppercase">Transaction details</div>
+            <div className="transactionItemsContainer">
+              {transactions.map((item, index) => {
+                const { date, amount, description, category, transactionType } =
+                  item;
+                return (
+                  <div className="transactionItem" key={index}>
+                    <div className="leftSideContainer">
+                      <div className="transaction-item-date font-normal text-xl">
+                        {moment(date).format("D dddd, MMMM  Y")}
+                      </div>
+                      <div className="pair">
+                        <IconContext.Provider
+                          value={{
+                            color:
+                              transactionType === "Income"
+                                ? "#00766C"
+                                : "#760000",
+                            className:
+                              "global-class-name transaction-item-icon",
+                          }}
+                        >
+                          <div>
+                            <BsCircleFill />
+                          </div>
+                        </IconContext.Provider>
+                        <div className="transaction-item-category">
+                          {category}
                         </div>
-                      </IconContext.Provider>
-                      <div className="transaction-item-category">
-                        {category}
                       </div>
-                    </div>
 
-                    <div className="transaction-item-description ">
-                      {description}
+                      <div className="transaction-item-description ">
+                        {description}
+                      </div>
+                    </div>
+                    <div className="rightSideContainer">
+                      {transactionType === "Expense" ? (
+                        <div className="expense text-lg font-normal">
+                          {"- " + amount}
+                        </div>
+                      ) : (
+                        <div className="income text-lg font-normal">
+                          {"+ " + amount}
+                        </div>
+                      )}
+                      <HiOutlineTrash size={25} className="ml-2 trash" />
                     </div>
                   </div>
-                  <div className="rightSideContainer">
-                    {transactionType === "Expense" ? (
-                      <div className="expense text-lg font-normal">
-                        {"- " + amount}
-                      </div>
-                    ) : (
-                      <div className="income text-lg font-normal">
-                        {"+ " + amount}
-                      </div>
-                    )}
-                    <HiOutlineTrash size={25} className="ml-2 trash" />
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
-        </div>
+        </>
       ) : (
         <div className="other">
           <div className="otherContents">
