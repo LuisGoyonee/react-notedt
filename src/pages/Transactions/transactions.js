03cf9d56dbd4364eeef5e598100f43cc89c6bb97
Franz Nico Testado <fnotestado@gmail.com>
2022-05-04 16:52:55 +0800

    feat: not transactions page

--- src/pages/Transactions/transactions.js
+++ src/pages/Transactions/transactions.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { retrieveTransactions } from "../../scripts/local-storage";
 import "./transactions.css";
+import noTransactions from "../../images/no-transactions.png"
 
 const Transactions = () => {
   let transactions = retrieveTransactions();
@@ -9,7 +10,17 @@ const Transactions = () => {
       {transactions ? (
         <div className="transactionContainer">Hi</div>
       ) : (
-        <div className="other">Hello</div>
+        <div className="other">
+          <div className="otherContents">
+              <img class="noTransactionsVector" src={noTransactions} alt="" />
+            <div>
+              <p class="noTransactions">NO TRANSACTIONS</p>
+            </div>
+            <div>
+                <p class="noTransactionsDescription">You do not have any transactions yet. Click on the “Add Transactions” button to start tracking your budget.</p>
+            </div>
+          </div>
+        </div>
       )}
     </>
   );
