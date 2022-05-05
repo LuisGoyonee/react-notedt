c10e7046b6c0b655d2710ea4ec8e23689bcf8187
Franz Nico Testado <fnotestado@gmail.com>
2022-05-05 09:28:49 +0800

    Revert "fix: exclude lastKey in the valid transaction lists"
    This reverts commit 05fe9194242ea6d056f3cfb1967d3c9545cd0a26.

--- src/scripts/local-storage.js
+++ src/scripts/local-storage.js
@@ -1,59 +1,58 @@
 export function store(data) {
-    if (localStorage.length === 0) {
-        localStorage.setItem("lastKey", "0");
-        localStorage.setItem("0", JSON.stringify(data));
-    } else {
-        var lastKey = localStorage.getItem("lastKey");
-        var newKey = parseInt(lastKey) + 1;
-        localStorage.setItem("lastKey", newKey);
-        localStorage.setItem(newKey, JSON.stringify(data));
-    }
+  if (localStorage.length === 0) {
+    localStorage.setItem("lastKey", "0");
+    localStorage.setItem("0", JSON.stringify(data));
+  } else {
+    var lastKey = localStorage.getItem("lastKey");
+    var newKey = parseInt(lastKey) + 1;
+    localStorage.setItem("lastKey", newKey);
+    localStorage.setItem(newKey, JSON.stringify(data));
+  }
 }
 
 export function retrieveTransactions() {
-    var transactions = [];
-    var lastKey = localStorage.getItem("lastKey");
-    for (var i = 0; i <= lastKey; i++) {
-        if (localStorage.getItem(i)) {
-            transactions.push(JSON.parse(localStorage.getItem(i)));
-        } else {
-            break;
-        }
+  var transactions = [];
+  var lastKey = localStorage.getItem("lastKey");
+  for (var i = 0; i <= lastKey; i++) {
+    if (localStorage.getItem(i)) {
+      transactions.push(JSON.parse(localStorage.getItem(i)));
+    } else {
+      break;
     }
-    return transactions;
+  }
+  return transactions;
 }
 
 export function checker() {
-    var lastKey = localStorage.getItem("lastKey");
-    var transactions = null;
-    var hasValidItems = localStorage.length > 1; // last key excluded
-    if (hasValidItems) {
-        transactions = [];
-        for (let i = 1; i <= lastKey; i++) {
-            if (localStorage[i] !== null) {
-                transactions.push(JSON.parse(localStorage[i]));
-            }
-        }
+  var lastKey = localStorage.getItem("lastKey");
+  var transactions = null;
+  if (localStorage.length > 0) {
+    transactions = [];
+    for (let i = 1; i <= lastKey; i++) {
+      if (localStorage[i] !== null) {
+        transactions.push(JSON.parse(localStorage[i]));
+      }
     }
-    return transactions;
+  }
+  return transactions;
 }
 
 export function populateData() {}
 
 export function getTotalSavings() {
-    var total = 0;
-    var transactions = retrieveTransactions();
-    if (transactions !== null) {
-        transactions.forEach((item) => {
-            if (item.amount > 0) {
-                if (item.transactionType === "Income") {
-                    total += parseFloat(item.amount);
-                } else if (item.transactionType === "Expense") {
-                    total -= parseFloat(item.amount);
-                }
-            }
-            return total;
-        });
-    }
-    return total;
+  var total = 0;
+  var transactions = retrieveTransactions();
+  if (transactions !== null) {
+    transactions.forEach((item) => {
+      if (item.amount > 0) {
+        if (item.transactionType === "Income") {
+          total += parseFloat(item.amount);
+        } else if (item.transactionType === "Expense") {
+          total -= parseFloat(item.amount);
+        }
+      }
+      return total;
+    });
+  }
+  return total;
 }
